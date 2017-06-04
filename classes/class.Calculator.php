0174311c3a1ffea699d75e7f2eadd1a5c9c219c8
Mike Ciavarella <mciav5@gmail.com>
2017-06-03 22:52:32 -0400

    Adding MACD

--- classes/class.Calculator.php
+++ classes/class.Calculator.php
@@ -20,11 +20,11 @@ class Calculator{
 	public function EMA($closes){
 		$n = count($closes);
 		$previous = $this->SMA($closes);
-		$multiplier = (2.0 / ($n + 1) );
+		$multiplier = (2.0 / ($n + 1.0) );
 		$EMA = 0.0;
 		for($i = $n-1; $i>-1; $i-=1){
 			$close = $closes[$i];
-			$EMA = ($close * $multiplier) + ($previous * (1 - $multiplier));
+			$EMA = ($close * $multiplier) + ($previous * (1.0 - $multiplier));
 			$previous = $EMA;
 		}
 		return $EMA;
@@ -33,15 +33,30 @@ class Calculator{
 	//This EMA calculation uses the oldest closing price as the seed
 	public function EMA2($closes){
 		$n = count($closes);
-		$multiplier = (2.0 / ($n + 1) );
+		$multiplier = (2.0 / ($n + 1.0) );
 		$previous = $closes[$n-1];
 		$EMA = 0.0;
 		for($i = $n-1; $i>-1; $i-=1){
 			$close = $closes[$i];
-			$EMA = ($close * $multiplier) + ($previous * (1 - $multiplier));
+			$EMA = ($close * $multiplier) + ($previous * (1.0 - $multiplier));
 			$previous = $EMA;
 		}
 		return $EMA;
+	}
+
+	public function MACD($closes, $short_cnt, $long_cnt, $signal_cnt){
+		$short = $long = $signal = [];
+		$MACD = 0.0;
+		for($i=$signal_cnt-1; $i>-1; $i-=1){
+			$short = array_slice($closes, $i, $short_cnt);
+			$long = array_slice($closes, $i, $long_cnt);
+
+			$MACD = $this->EMA($short) - $this->EMA($long);
+			array_unshift($signal,$MACD);
+		}
+		$cur_signal = $this->EMA($signal);
+		return $MACD - $cur_signal;
+
 	}
 }
 ?>
---
+++ macd_main.php
@@ -0,0 +1,73 @@
+<?php
+require_once "classes/class.Eddie.php";
+require_once "classes/class.Calculator.php";
+require_once "classes/class.Logger.php";
+
+date_default_timezone_set('America/New_York');
+
+$eddie = new Eddie("GDAX");
+$calc = new Calculator;
+
+$now = date(DATE_ATOM, time());
+$limit_time = date(DATE_ATOM, time() - (MACD_CROSSOVER_CANDLE_WIDTH*(LONG_TERM_MACD_PERIOD + MACD_SIGNAL_PERIOD)));
+
+$candles = $eddie->getCandles($limit_time, $now, MACD_CROSSOVER_CANDLE_WIDTH);
+var_dump($candles);
+
+//Extract closing prices
+//Omit current candle from these calculations as it is too volatile
+$closes = array();
+for($i=1;$i<LONG_TERM_MACD_PERIOD+MACD_SIGNAL_PERIOD;$i+=1){
+	$closes[] = $candles[$i][4];
+}
+
+//Calculate MACD Crossover
+$crossover_value = $calc->MACD($closes, SHORT_TERM_MACD_PERIOD, LONG_TERM_MACD_PERIOD, MACD_SIGNAL_PERIOD);
+echo "MACD crossover value: " . $crossover_value . "\n";
+
+$raw_accounts = $eddie->getAccounts();
+$accounts = array();
+$eth_account = null;
+$usd_account = null;
+foreach($raw_accounts as $account){
+	$accounts[$account->currency] = $account;
+}
+
+echo "\n";
+
+$side = "";
+$size = 0;
+if($crossover_value >= 0){
+	//Buy
+	echo "MACD > Signal: Buying procedure triggered\n";
+	$side = "buy";
+	$size = $accounts["USD"]->balance;
+	if($accounts["USD"]->balance  < 0.01){ //Minimum transaction = 1 cent
+		echo "Exiting - No funds available.\n";
+		exit(0);
+	}
+}
+else{
+	//Sell
+	echo "MACD < Signal: Selling procedure triggered\n";
+	$side = "sell";
+	$size = $accounts["ETH"]->balance;
+	if($accounts["ETH"]->balance < 0.01){ //Minimum transaction = 0.01 ETH
+		echo "Exiting - No funds available.\n";
+		exit(0);
+	}
+}
+$price = $eddie->placeOrder($side, $side);
+if($price < 0){
+	echo "Exiting - failed to fulfill an order in a reasonable time\n";
+	exit(0);
+}
+
+//Log transaction
+if($side == "buy"){
+	$size = $size/$price;
+}
+$log = new Logger("log/log.csv");
+$log->logTransaction($side, $size, $price);
+
+?>
