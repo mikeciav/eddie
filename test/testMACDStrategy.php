cef725b37643330e144310adc3ed36b539423f95
Ciavarella <AAB8259@carefirst.com>
2017-06-19 08:16:00 -0400

    Adding MACD Strategy tester

---
+++ test/testMACDStrategy.php
@@ -0,0 +1,34 @@
+<?php
+require_once "private/defines.php";
+
+require_once PROJ_ROOT . "/classes/class.Eddie.php";
+require_once PROJ_ROOT . "/classes/class.Calculator.php";
+require_once PROJ_ROOT . "/classes/class.Logger.php";
+
+require_once PROJ_ROOT . "/strategies/StrategyInterface.php";
+require_once PROJ_ROOT . "/strategies/class.MACD.php";
+
+date_default_timezone_set('America/New_York');
+
+sleep(5); //A small offset to allow the exchange to generate the candle for the last period
+
+$execute_order_flag = ($argc > 1 && $argv[1] == "true") ? true : false;
+$mid_candle = false;
+
+mainProc($execute_order_flag, $mid_candle);
+//If this returns, we know an order has been placed.
+//We can re-evaluate the position mid-candle for more accuracy
+sleep(5);
+$mid_candle = true;
+mainProc($execute_order_flag, $mid_candle);
+
+
+function mainProc($execute_order_flag, $mid_candle){
+	$eddie = new Eddie("GDAX");
+
+	//Replace this class with whichever strategy you would like to use
+	$strategy = new MACDStrategy($eddie, $mid_candle);
+	$action = $strategy->evaluate();
+}
+
+?>
