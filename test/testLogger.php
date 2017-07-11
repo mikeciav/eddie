5d12f00a036ef642aedb6f820286b06c380dbb18
Mike Ciavarella <mciav5@gmail.com>
2017-07-11 18:57:36 -0400

    Fixing a test

--- test/testLogger.php
+++ test/testLogger.php
@@ -1,4 +1,6 @@
 <?php
+require_once "private/defines.php";
+
 require_once PROJ_ROOT . "/classes/class.Eddie.php";
 require_once PROJ_ROOT . "/classes/class.Logger.php";
 
@@ -8,6 +10,7 @@ $eddie = new Eddie("GDAX");
 
 
 $log = new Logger("log/log.csv");
-$log->logTransaction("buy", 2.0, 100.0);
+$log->logTransaction("buy", 2.0, 100.0, false);
+$log->logTransaction("sell", 1.0, 105.0, true);
 
 ?>
